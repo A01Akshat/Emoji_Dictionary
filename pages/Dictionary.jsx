e44f73477fcfadfed96e766b5e66fb445f721d66
A01Akshat <104577005+A01Akshat@users.noreply.github.com>
2023-06-25 21:11:07 +0530

    Update Dictionary.jsx

--- pages/Dictionary.jsx
+++ pages/Dictionary.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { emojis } from '@/public/emojis';
-
+import idx from '../styles/index.module.css';
 
 
 const Dictionary = () => {
+  const [data,setdata]=useState([]);
+  const [search,setsearch]=useState('');
 
-  
+
+  useEffect(()=>{
+    const newData = emojis.filter(emojis => emojis.name.toLowerCase().includes(search.toLowerCase()));
+    setdata(newData);
+},[search])
+
+
+
   return (
     <>
-    <div>{emojis.map((item,index)=>{
+    <input className={idx.first} style={{marginLeft:"34rem",marginTop:"1rem",height:"2rem",borderRadius:"20px"}} size={20} placeholder="Search Emoji" value={search} onChange={(e)=>setsearch(e.target.value)} />
+    <div>{data.map(emojis=>{
         return(<>
         <div style={{display:"flex",justifyContent:"center",flexDirection:"row"}}>
-            <h2>{emojis[index].emoji}:</h2>
-            <h2 style={{fontFamily:"cursive",fontSize:"18px",paddingTop:"0.6rem",paddingLeft:"6px"}}>{emojis[index].name}</h2>
+        
+            <h2>{emojis.emoji}:</h2>
+            <h2 style={{fontFamily:"cursive",fontSize:"18px",paddingTop:"0.6rem",paddingLeft:"6px"}}>{emojis.name}</h2>
             </div>
         </>)
     })}</div>
